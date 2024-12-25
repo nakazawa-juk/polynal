664f26e19789b88e4fe4fd892561a9e367023d55
Nakazawa-Jukiya <nakazawamusc@gmail.com>
2024-12-25 15:52:54 +0900

    Initial Commit

---
+++ Polynal/PolynalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolynalApp.swift
+//  Polynal
+//
+//  Created by Nakazawa Jakiya on 2024-12-25.
+//
+
+import SwiftUI
+
+@main
+struct PolynalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
